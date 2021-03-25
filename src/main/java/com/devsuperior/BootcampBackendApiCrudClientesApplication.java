818ce3e0ca723ce3302579214e180ba9e6a4581e
MarcosRogerioFerreiraIFMS <marcos.rogerio@ifms.edu.br>
2021-03-25 11:19:24 -0400

    Criação do Projeto

---
+++ src/main/java/com/devsuperior/BootcampBackendApiCrudClientesApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampBackendApiCrudClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampBackendApiCrudClientesApplication.class, args);
+	}
+
+}
